6ace1f41572b534e48f00fb012dc96e6d3605c35
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-08-20 00:28:10 +0600

    Initial commit

--- android/app/src/main/kotlin/com/RustamKeneev/e_commerce_flutter/e_commerce_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/RustamKeneev/e_commerce_flutter/e_commerce_flutter/MainActivity.kt
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com.RustamKeneev.e_commerce_flutter
 =======
 package com.RustamKeneev.e_commerce_flutter.e_commerce_flutter
 >>>>>>> 569b799 (Resolved merge conflicts and added assets folder)
+=======
+package com.RustamKeneev.e_commerce_flutter
+>>>>>>> 55844d8 (Initial commit)
 
 import io.flutter.embedding.android.FlutterActivity
 
