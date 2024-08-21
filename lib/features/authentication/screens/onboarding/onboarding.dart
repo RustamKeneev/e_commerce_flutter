6bffbe106c079bf7bfd5e91e0a52c1dd891c1e15
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-08-22 00:39:47 +0600

    Done setting Flutter Onboarding Screen

---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,95 @@
+import 'package:e_commerce_flutter/utils/constants/image_strings.dart';
+import 'package:e_commerce_flutter/utils/constants/text_strings.dart';
+import 'package:e_commerce_flutter/utils/device/device_utility.dart';
+import 'package:e_commerce_flutter/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import '../../../utils/constants/sizes.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          ///Horizontal Scrollable Pages
+          PageView(
+            children: const [
+              OnBoardingPage(
+                  image: TImages.onBoardingImage1,
+                  title: TTexts.onBoardingTitle1,
+                  subTitle: TTexts.onBoardingSubTitle1),
+              OnBoardingPage(
+                  image: TImages.onBoardingImage2,
+                  title: TTexts.onBoardingTitle2,
+                  subTitle: TTexts.onBoardingSubTitle2),
+              OnBoardingPage(
+                  image: TImages.onBoardingImage3,
+                  title: TTexts.onBoardingTitle3,
+                  subTitle: TTexts.onBoardingSubTitle3)
+            ],
+          ),
+
+          ///Skip Button
+          const OnBoardSkip()
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardSkip extends StatelessWidget {
+  const OnBoardSkip({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        top: TDeviceUtils.getAppBarHeight(),
+        right: TSizes.defaultSpace,
+        child: TextButton(
+          onPressed: () {},
+          child: const Text("Skip"),
+        )
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+              width: THelperFunctions.screenWidth() * 0.8,
+              height: THelperFunctions.screenHeight() * 0.6,
+              image: AssetImage(image)),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
