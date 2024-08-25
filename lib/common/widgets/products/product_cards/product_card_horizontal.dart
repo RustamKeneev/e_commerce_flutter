6cfab119db2d99a85e7ad96f8cb381ac405763d2
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-08-25 17:42:20 +0600

    Done display Gridview  with Product Card Design

---
+++ lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TProductCardVertical extends StatelessWidget{
+  const TProductCardVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
